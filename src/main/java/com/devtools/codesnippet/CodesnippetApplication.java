d9b2556264862913f767d9d2782dd944495e29ff
Bulagonda <amrutha.v.bulagonda@accenture.com>
2025-01-07 16:31:17 +0530

    added all files

---
+++ src/main/java/com/devtools/codesnippet/CodesnippetApplication.java
@@ -0,0 +1,13 @@
+package com.devtools.codesnippet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CodesnippetApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CodesnippetApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/devtools/codesnippet/CodesnippetApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devtools.codesnippet;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CodesnippetApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
